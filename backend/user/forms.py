2413b159d31aa8009c2cf5029008fed10d3f0aa2
Tran Duy Hung <mspotify208@gmail.com>
2023-09-25 16:32:44 +0700

    add profile form

--- backend/user/forms.py
+++ backend/user/forms.py
@@ -8,3 +8,10 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'email',
                   'username', 'password1', 'password2']
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'username', 'email', 'short_intro', 'bio', 'location', 'social_github',
+                  'social_twitter', 'social_linkedin', 'social_youtube', 'social_website']
