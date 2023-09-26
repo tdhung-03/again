0e2c5dede9bd818db878606337b8049664b73112
Tran Duy Hung <mspotify208@gmail.com>
2023-09-26 08:58:09 +0700

    add static

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("register/", create_user, name="register"),
     path("login/", login_user, name="login"),
     path("logout/", logout_user, name="logout"),
-    path("account", account, name="account"),
+    path("account/", account, name="account"),
 ]
